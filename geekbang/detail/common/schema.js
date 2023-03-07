e1e7f81deef7e27e5cc2b39dc65f6d0b2b474968
ZhaoCe <17631807110@163.com>
2023-03-07 17:52:14 +0800

    fix: 详情页修改

--- geekbang/detail/common/schema.js
+++
@@ -1,9 +0,0 @@
-const protoBuf = require("protocol-buffers");
-const fs = require("fs");
-
-const schemas = protoBuf(fs.readFileSync(`${__dirname}/detail.proto`));
-
-modules.exports = {
-  columnRequest: schemas.ColumnRequest,
-  columnResponse: schemas.ColumnResponse
-};
